97e4645f9120cc50095bbd527a1fb18619417ac2
JaVD054 <jeedu124@gmail.com>
2025-08-02 12:27:32 +0530

    signed multiplicaton implemented

--- multiplcation/multiply.v
+++ multiplcation/multiply.v
@@ -1,31 +1,49 @@
 module multiply #(
-    WIDTH = 8
+    parameter WIDTH = 8
 ) (
     input clkin,
     input [WIDTH-1:0] a_in,
     input [WIDTH-1:0] b_in,
     output reg ready,
-    output reg [2*WIDTH-1:0] product
+    output sign,
+    output [2*WIDTH-3:0] product
 );
-    reg [WIDTH:0] a_temp;
-    reg [WIDTH:0] b_temp;
 
-    reg [WIDTH:0] a_op;
-    reg [2*WIDTH:0] b_op;
+    parameter MUL_WIDTH = WIDTH - 1; // Considering last bit is signed bit
+
+   
+
+    reg [WIDTH-1:0] a_temp;
+    reg [WIDTH-1:0] b_temp;
+
+    reg [2*MUL_WIDTH-1:0] a_op;
+    reg [MUL_WIDTH-1:0] b_op;
+
+    reg [2*MUL_WIDTH-1:0] product_op;
+
+    assign sign  = a_in[WIDTH-1] ^ b_in[WIDTH-1];
+    assign product = product_op;
 
     always @(posedge clkin) begin
-        if (b==0) begin
-            ready <= 1'b1;
-        end
-        else if (ready==1'b1 && a!=0 && b!=0) begin
+        
+        if (a_in != a_temp || b_in != b_temp) begin
+            a_temp <= a_in;
+            b_temp <= b_in;
+            a_op <= a_in[MUL_WIDTH-1:0];
+            b_op <= b_in[MUL_WIDTH-1:0];
             ready <= 1'b0;
-            product <= 0;
+            product_op <= 0;
         end
-        else if (ready != 0) begin
-            if (b[0] = 1'b1) begin
-                product <= product + a<<;
+        else if (!ready) begin
+            if (b_op != 0) begin
+                if (b_op[0] == 1'b1) begin
+                    product_op <= product_op + a_op;
+                end
+                a_op<=a_op<<1;
+                b_op <= b_op>>1;
+            end else begin
+                ready <= 1'b1;
             end
-            b <= b>>1
 
         end
     end
