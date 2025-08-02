75cb0ffe6ca55c42c417c425ae0cedc271053d2f
JaVD054 <jeedu124@gmail.com>
2025-08-03 00:37:44 +0530

    2's compliment

--- multiplcation/multiply.v
+++ multiplcation/multiply.v
@@ -6,51 +6,50 @@ module multiply #(
     input [WIDTH-1:0] b_in,
     output reg ready,
     output sign,
-    output [2*WIDTH-3:0] product
+    output reg [2*WIDTH-2:0] product
 );
 
-    parameter MUL_WIDTH = WIDTH - 1; // Considering last bit is signed bit
 
 
-   
 
-    reg [WIDTH-1:0] a_temp = 0;
-    reg [WIDTH-1:0] b_temp = 0;
+    reg [WIDTH-1:0] a_temp;
+    reg [WIDTH-1:0] b_temp;
 
-    reg [2*MUL_WIDTH-1:0] a_op;
-    reg [MUL_WIDTH-1:0] b_op;
+    reg [2*WIDTH-2:0] product_op;
+    reg [WIDTH-1:0] b_op;
+    reg [2*WIDTH-3:0] a_op;
 
-    reg [2*MUL_WIDTH-1:0] product_op;
+    initial begin
+        a_temp = 0;
+        b_temp = 0;
+        ready = 0;
+    end
+    
 
-    assign sign  = a_in[WIDTH-1] ^ b_in[WIDTH-1];
-    assign product = product_op;
+    assign sign = a_in[WIDTH-1] ^ b_in[WIDTH-1];
 
     always @(posedge clkin) begin
-        
         if (a_in != a_temp || b_in != b_temp) begin
             a_temp <= a_in;
             b_temp <= b_in;
-            if (a_in>b_in) begin
-                a_op <= a_in[MUL_WIDTH-1:0];
-                b_op <= b_in[MUL_WIDTH-1:0];
-            end else begin
-                a_op <= a_in[MUL_WIDTH-1:0];
-                b_op <= b_in[MUL_WIDTH-1:0];
-            end
-            ready <= 1'b0;
+
+            a_op <= a_in[WIDTH-1] ? -a_in : a_in;
+            b_op <= b_in[WIDTH-1] ? -b_in : b_in;
+
             product_op <= 0;
+            ready <= 0;
         end
         else if (!ready) begin
             if (b_op != 0) begin
-                if (b_op[0] == 1'b1) begin
+                if (b_op[0])
                     product_op <= product_op + a_op;
-                end
-                a_op<=a_op<<1;
-                b_op <= b_op>>1;
-            end else begin
-                ready <= 1'b1;
+                a_op <= a_op << 1;
+                b_op <= b_op >> 1;
             end
-
+            else begin
+                product <= sign ? -product_op : product_op;
+                ready <= 1;
+            end
         end
     end
 endmodule
