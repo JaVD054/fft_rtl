3ece5487101b935c371fcad06246c19789abe30f
JaVD054 <jeedu124@gmail.com>
2025-08-17 01:26:03 +0530

    didnt figure out how to generate binary tree like addition in verilog :TODO

--- multiplcation/multiply.v
+++ multiplcation/multiply.v
@@ -1,3 +1,5 @@
+// this module expect width as power of two
+
 module multiply #(
     parameter WIDTH = 16
 ) (
@@ -6,19 +8,34 @@ module multiply #(
     output wire [2*WIDTH-1:0] product
 );
 
+parameter STAGES = $clog2(WIDTH);
+parameter SUM_ARRAY_SIZE = 2*WIDTH -2;
+
 wire [2*WIDTH-1:0] partial [WIDTH-1:0];
-wire [2*WIDTH-1:0] sum [WIDTH:0];
 
-assign sum[0] = {2*WIDTH{1'b0}};
+wire [2*WIDTH-1:0] sum [SUM_ARRAY_SIZE:0];
 
-genvar i;
+ 
+genvar p;
 generate
-    for (i = 0; i < WIDTH; i = i + 1) begin : PARTIALS
-        assign partial[i] = b[i] ? (a << i) : {2*WIDTH{1'b0}};
-        assign sum[i+1] = sum[i] + partial[i];
+    for (p = 0; p < WIDTH; p = p + 1) begin : PARTIALS
+        assign partial[p] = b[p] ? (a << p) : {2*WIDTH{1'b0}};
+        assign sum[p] = partial[p];
     end
 endgenerate
 
-assign product = sum[WIDTH];
+genvar i;
+
+generate
+    for (i = WIDTH; i <=SUM_ARRAY_SIZE; i = i+1) begin : SUM
+
+        for ( j = offset; j< offset+WIDTH>>i; ) begin
+            
+        end
+        offset = j
+    end
+endgenerate
+
+assign product = sum[0];
 
 endmodule
